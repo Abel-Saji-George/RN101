1e5acf0c08d3f420136171c829ef91f3c1945ab6
Abel-Saji-George <66424337+Abel-Saji-George@users.noreply.github.com>
2022-01-24 00:14:54 +0530

    flat hei

--- BasicTutorial/App.js
+++ BasicTutorial/App.js
@@ -3,6 +3,7 @@ import React,{useState} from 'react';
 
 import {
     
+  FlatList,
   RefreshControl,
   ScrollView,
   StyleSheet,
@@ -15,18 +16,18 @@ import {
 const App = () => {
 
   const [Items,setItems]=useState([
-    {key:1,item:'Item 1'},
-    {key:2,item:'Item 2'},
-    {key:3,item:'Item 3'},
-    {key:4,item:'Item 4'},
-    {key:5,item:'Item 5'},
-    {key:6,item:'Item 6'},
-    {key:7,item:'Item 7'},
-    {key:8,item:'Item 8'},
-    {key:9,item:'Item 9'},
-    {key:10,item:'Item 10'},
-    {key:11,item:'Item 11'},
-    {key:12,item:'Item 12'},
+    {name:'Item 1'},
+    {name:'Item 2'},
+    {name:'Item 3'},
+    {name:'Item 4'},
+    {name:'Item 5'},
+    {name:'Item 6'},
+    {name:'Item 7'},
+    {name:'Item 8'},
+    {name:'Item 9'},
+    {name:'Item 10'},
+    {name:'Item 11'},
+    {name:'Item 12'},
 
   ]
 )
@@ -34,30 +35,40 @@ const [Refreshing,setRefreshing]=useState(false)
 
 const onRefresh=()=>{
   setRefreshing(true);
-  setItems([...Items,{key:69,item:'Item 69'}]);
+  setItems([...Items,{name:'Item 69'}]);
   setRefreshing(false);
 }
 
   return (
-  <ScrollView style={styles.body} 
-   refreshControl={
-     <RefreshControl
-     refreshing={Refreshing}
-     onRefresh={onRefresh}
-     colors={['#0000ffff']}
-     />
-   }
-  >
-  {
-    Items.map((object)=>{
-      return(
-      <View style={styles.item} key={object.key}>
-           <Text style={styles.text}>{object.item}</Text>
-     </View>
-      );
-    })
-  }
-  </ScrollView>
+    <FlatList
+    keyExtractor={(item,index)=>index.toString()}
+    data={Items}
+    renderItem={
+      ({item})=>(
+        <View style={styles.item}>
+               <Text style={styles.text}>{item.name}</Text>
+        </View>
+      )
+    }
+    refreshControl={
+         <RefreshControl
+         refreshing={Refreshing}
+         onRefresh={onRefresh}
+         colors={['#0000ffff']}
+         />
+       }
+  />
+  // <ScrollView style={styles.body} 
+  //
+  // >
+  // {
+  //   Items.map((object)=>{
+  //     return(
+  //
+  //     );
+  //   })
+  // }
+  // </ScrollView>
 
   );
 };
