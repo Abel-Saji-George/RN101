8f25f396aba4fe947d8ffdb16ff09f9dd863b6ac
Abel-Saji-George <66424337+Abel-Saji-George@users.noreply.github.com>
2022-01-23 16:36:46 +0530

    simple counter app created

--- BasicTutorial/App.js
+++ BasicTutorial/App.js
@@ -9,29 +9,28 @@ import {
   Text,
   View,
 } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+
  
 
 
 const App = () => {
 
-const [name,setName]=useState('Initial State' )
-const [session,setSession]=useState({number:6,title:'Before update'})
-const [current,setCurrent]=useState(true)
+const [by1counter,setBy1counter]=useState(0)
+const [by5counter,setBy5counter]=useState(0)
+
 const onClickHandle=()=>{
-  setName('Final State')
-  setSession({number:7,title :'After update'})
-  setCurrent(false)
+  setBy1counter(by1counter+1)
+  setBy5counter(by5counter+5)
+
 }
   return (
 <View style={styles.body}>
-  <Text style={styles.text}>{name} </Text>
-  <Text style={styles.text}>Session number: {session.number}</Text>
- <Text style={styles.text}> About: {session.title} </Text>
- <Text style={styles.text}> {current?'Current session':'Next Session'} </Text>
+  <Text style={styles.text}>{by5counter} </Text>
 
 
-  <Button title='State Change check' onPress={onClickHandle}></Button>
+  <Button title='  ADD  ' onPress={onClickHandle} ></Button>
+  <Text style={styles.text}>You clicked {by1counter} times</Text>
+
 </View>    
   );
 };
