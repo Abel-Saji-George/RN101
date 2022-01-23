43b0340421efcf5623e6b9e7b1c185d5d7d4a46e
Abel-Saji-George <66424337+Abel-Saji-George@users.noreply.github.com>
2022-01-23 21:37:07 +0530

    just flexing

--- BasicTutorial/App.js
+++ BasicTutorial/App.js
@@ -2,9 +2,7 @@
 import React,{useState} from 'react';
 
 import {
-  
-  Button,
-  
+    
   StyleSheet,
   Text,
   View,
@@ -21,31 +19,132 @@ setName('Styling is Completed!!!')
 }
   return (
 <View style={styles.body}>
- <Text style={styles.text}>{name}</Text>
-<View style={styles.button}>
-<Button title='   Update State   ' onPress={onClickHandle} ></Button>
 
-</View>
+  <View style={styles.row}>
+
+    <View style={styles.view1}>
+    <Text style={styles.text}>1</Text>
+    </View>
+
+    <View style={styles.view2}>
+    <Text style={styles.text}>2</Text>
+    </View>
+
+    <View style={styles.view3}>
+    <Text style={styles.text}>3</Text>
+    </View>
+
+  </View>
+
+
+
+  <View style={styles.row}>
+
+    <View style={styles.view4}>
+    <Text style={styles.text}>4</Text>
+    </View>
+
+  </View>
+
+
+  <View style={styles.row}>
+
+    <View style={styles.view5}>
+    <Text style={styles.text}>5</Text>
+    </View>
+
+  </View>
+
+
+  <View style={styles.row1}>
+
+    <View style={styles.view6}>
+    <Text style={styles.text}>6</Text>
+    </View>
+    <View style={styles.view7}>
+    <Text style={styles.text}>7</Text>
+    </View>
+
+  </View>
+
+
+
+
 </View>    
+
+
+
   );
 };
 
 const styles = StyleSheet.create({
+  row:{
+    flexDirection:'row',
+    
+
+  },
+  row1:{
+    flex:1,
+    flexDirection:'row',
+    alignItems:'stretch'
+
+
+  },
  body:{
-   flex:1,
-  //  width:'100%',
-  //  height:'50%',
-  // width:300,
-  // height:500,
-   backgroundColor:'#0fffff',
-   alignItems:'center',
-   justifyContent:'center',
-   borderWidth:10,
-   borderColor:'#ff00ff',
-   borderRadius:10,
-  //  margin:40,
+   flex:1, 
+   flexDirection:'column',
+
+   backgroundColor:'#0000ffff',
+   alignItems:'center', 
+   justifyContent:'flex-start',
+  
 
  },
+ view1:{
+flex:1,
+  backgroundColor:'#00ffffff',
+  alignItems:'center', 
+  justifyContent:'center'
+ },
+
+ view2:{
+ flex:2,
+
+  backgroundColor:'#ff00ff',
+  alignItems:'center', 
+  justifyContent:'center'
+ },
+  view3:{
+    flex:3,
+
+  backgroundColor:'#fff00f',
+  alignItems:'center', 
+  justifyContent:'center'
+ },
+ view4:{
+  flex:1,
+    backgroundColor:'#ff0000',
+    alignItems:'center', 
+    justifyContent:'center'
+   },
+   view5:{
+    flex:1,
+      backgroundColor:'#0fff0f',
+      alignItems:'center', 
+      justifyContent:'center'
+     },
+     view6:{
+      flex:1,
+        backgroundColor:'#ffffff',
+        alignItems:'center', 
+        justifyContent:'center'
+       },
+       view7:{
+        flex:1,
+          backgroundColor:'#0000ff',
+          alignItems:'center', 
+          justifyContent:'center'
+         },
  text:{
     color:'#000000',
     fontSize:30,
@@ -54,10 +153,9 @@ const styles = StyleSheet.create({
     margin:10
 
  },
- button:{
-height:60,
-width:200
- }
+ 
+
 });
 
 export default App;
+ 
