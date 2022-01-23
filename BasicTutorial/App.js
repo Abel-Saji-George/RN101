42fbe8ccf87963886bf0c39803066f5d1c28678d
Abel-Saji-George <66424337+Abel-Saji-George@users.noreply.github.com>
2022-01-23 16:01:49 +0530

    StateHook done

--- BasicTutorial/App.js
+++ BasicTutorial/App.js
@@ -1,13 +1,12 @@
 
-import React from 'react';
+import React,{useState} from 'react';
 
 import {
   
   Button,
-  Linking,
+  
   StyleSheet,
   Text,
-  
   View,
 } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
@@ -16,12 +15,23 @@ import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
 
-
-
+const [name,setName]=useState('Initial State' )
+const [session,setSession]=useState({number:6,title:'Before update'})
+const [current,setCurrent]=useState(true)
+const onClickHandle=()=>{
+  setName('Final State')
+  setSession({number:7,title :'After update'})
+  setCurrent(false)
+}
   return (
 <View style={styles.body}>
-  <Text style={styles.text}>This is my profile Link</Text>
-  <Button title='LinkedIn' onPress={()=>{Linking.openURL("https://www.linkedin.com/in/abel-saji-george-b416591ab/")}}></Button>
+  <Text style={styles.text}>{name} </Text>
+  <Text style={styles.text}>Session number: {session.number}</Text>
+ <Text style={styles.text}> About: {session.title} </Text>
+ <Text style={styles.text}> {current?'Current session':'Next Session'} </Text>
+
+
+  <Button title='State Change check' onPress={onClickHandle}></Button>
 </View>    
   );
 };
